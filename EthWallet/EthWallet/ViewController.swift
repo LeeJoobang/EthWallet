9f58738424e94e5f4282dba74bb964545589b30f
Joobang <hii5074@gmail.com>
2023-06-02 02:11:11 +0900

    project init

---
+++ EthWallet/EthWallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EthWallet
+//
+//  Created by Joobang on 2023/06/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
